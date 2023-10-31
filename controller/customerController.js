e6b9f712800f5c7b998dcf09b90ff038de0b003a
NimnaKs <nimnakaveesha123456@gmail.com>
2023-10-31 22:37:57 +0530

    Adding item functions

--- controller/customerController.js
+++ controller/customerController.js
@@ -12,6 +12,9 @@ let address = $('#address');
 let contact = $('#contact');
 let email = $('#email');
 
+/*Validation*/
+const emailPattern = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+const mobilePattern = new RegExp("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$");
 
 /*Function to generate the next customer ID*/
 function generateCustomerId() {
@@ -27,12 +30,39 @@ function generateCustomerId() {
 /*Auto-generate the customer ID when navigating to the main section*/
 $('#customer_page').on('click', function() {
     customer_id.val(generateCustomerId());
+    populateCustomerTable();
 });
 
 /*Reset Columns*/
 function resetColumns() {
     reset.click();
     customer_id.val(generateCustomerId());
+}
+
+/*Validation*/
+function validation(value,message,test) {
+    if(!value){
+        showValidationError('Null Input','Input '+message);
+        return false;
+    }
+    if(test===null){
+        return true;
+    }
+    if(!test){
+        showValidationError('Invalid Input','Invalid Input '+message);
+        return false;
+    }
+    return true;
+}
+
+/*Show Validation Error*/
+function showValidationError(title, text) {
+    Swal.fire({
+        icon: 'error',
+        title: title,
+        text: text,
+        footer: '<a href="">Why do I have this issue?</a>'
+    });
 }
 
 /*Customer Form Submit*/
@@ -44,24 +74,36 @@ submit.on('click', () => {
     let contactValue = contact.val().trim();
     let emailValue = email.val().trim();
 
-    let customer = new CustomerModel(
-        customerIdValue,
-        nameValue,
-        addressValue,
-        contactValue,
-        emailValue
-    );
+    if(
+        validation(nameValue, "customer name", null) &&
+        validation(addressValue, "Address", null) &&
+        validation(contactValue, "Contact", mobilePattern.test(contactValue)) &&
+        validation(emailValue,"Email",emailPattern.test(emailValue))){
+        let customer = new CustomerModel(
+            customerIdValue,
+            nameValue,
+            addressValue,
+            contactValue,
+            emailValue
+        );
 
-    customer_db.push(customer);
+        Swal.fire(
+            'Save Successfully !',
+            'Successful',
+            'success'
+        )
 
-    populateStudentTable();
+        customer_db.push(customer);
 
-    resetColumns();
+        populateCustomerTable();
+
+        resetColumns();
+    }
 
 });
 
 /*Populate Table*/
-function populateStudentTable(){
+function populateCustomerTable(){
     $('tbody').eq(0).empty();
     customer_db.map((customer) => {
         $('tbody').eq(0).append(
@@ -100,18 +142,32 @@ update.on('click', () => {
     let contactValue = contact.val().trim();
     let emailValue = email.val().trim();
 
-    customer_db.map((customer) => {
-        if (customer.customer_id === customerIdValue){
-            customer.name = nameValue;
-            customer.address = addressValue;
-            customer.contact = contactValue;
-            customer.email = emailValue;
-        }
-    });
+    if(
+        validation(nameValue, "customer name", null) &&
+        validation(addressValue, "Address", null) &&
+        validation(contactValue, "Contact", mobilePattern.test(contactValue)) &&
+        validation(emailValue,"Email",emailPattern.test(emailValue))) {
 
-    populateStudentTable();
+        customer_db.map((customer) => {
+            if (customer.customer_id === customerIdValue) {
+                customer.name = nameValue;
+                customer.address = addressValue;
+                customer.contact = contactValue;
+                customer.email = emailValue;
+            }
+        });
 
-    resetColumns();
+        Swal.fire(
+            'Update Successfully !',
+            'Successful',
+            'success'
+        )
+
+        populateCustomerTable();
+
+        resetColumns();
+
+    }
 
 });
 
@@ -129,10 +185,29 @@ reset.on('click', function(e) {
 delete_btn.on('click', () => {
 
     let customerIdValue = parseInt(customer_id.val(), 10);
-    let index = customer_db.findIndex(customer => customer.customer_id === customerIdValue);
-    customer_db.splice(index, 1);
-    populateStudentTable();
-    reset.click();
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Delete'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let index = customer_db.findIndex(customer => customer.customer_id === customerIdValue);
+            customer_db.splice(index, 1);
+            populateCustomerTable();
+            reset.click();
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+    });
+
 
 });
 
---
+++ controller/itemController.js
@@ -0,0 +1,204 @@
+import {item_db} from "../db/db.js";
+import {ItemModel} from "../model/itemModel.js";
+
+let submit = $('#Item .btn-success').eq(0);
+let update = $('#Item .btn-primary').eq(0);
+let delete_btn = $('#Item .btn-danger').eq(0);
+let reset = $('#Item .btn-warning').eq(0);
+
+let itemCode = $('#item_code');
+let itemName = $('#item_name');
+let price = $('#price');
+let QtyOnHand = $('#QtyOnHand');
+
+
+/*Validation*/
+
+/*Function to generate the next customer ID*/
+function generateItemCode() {
+    let itCode = 0;
+    for (let i = 0; i < item_db.length; i++) {
+        if (item_db[i].item_code > itCode) {
+            itCode = item_db[i].item_code;
+        }
+    }
+    return itCode + 1;
+}
+
+/*Auto-generate the customer ID when navigating to the main section*/
+$('#item_page').on('click', function() {
+    itemCode.val(generateItemCode());
+    populateItemTable();
+});
+
+/*Reset Columns*/
+function resetColumns() {
+    reset.click();
+    itemCode.val(generateItemCode());
+}
+
+/*Validation*/
+function validation(value,message,test) {
+    if(!value){
+        showValidationError('Null Input','Input '+message);
+        return false;
+    }
+    if(test===null){
+        return true;
+    }
+    if(!test){
+        showValidationError('Invalid Input','Invalid Input '+message);
+        return false;
+    }
+    return true;
+}
+
+/*Show Validation Error*/
+function showValidationError(title, text) {
+    Swal.fire({
+        icon: 'error',
+        title: title,
+        text: text,
+        footer: '<a href="">Why do I have this issue?</a>'
+    });
+}
+
+/*Customer Form Submit*/
+submit.on('click', () => {
+
+    let itemCodeValue = parseInt(itemCode.val(), 10);
+    let itemNameValue = itemName.val().trim();
+    let priceValue = parseFloat(price.val());
+    let qtyOnHandValue = parseInt(QtyOnHand.val(), 10);
+
+    if(
+        validation(itemNameValue, "item name", null) &&
+        validation(priceValue, "Price", null) &&
+        validation(qtyOnHandValue, "Qty On Hand",null)){
+        let item = new ItemModel(
+            itemCodeValue,
+            itemNameValue,
+            priceValue,
+            qtyOnHandValue
+        );
+
+        Swal.fire(
+            'Save Successfully !',
+            'Successful',
+            'success'
+        )
+
+        item_db.push(item);
+
+        populateItemTable();
+
+        resetColumns();
+    }
+
+});
+
+/*Populate Table*/
+function populateItemTable(){
+    $('tbody').eq(0).empty();
+    item_db.map((item) => {
+        $('tbody').eq(0).append(
+            `<tr>
+                <th scope="row">${item.item_code}</th>
+                <td>${item.item_name}</td>
+                <td>${item.price}</td>
+                <td>${item.qty_on_hand}</td>
+            </tr>`
+        );
+    });
+}
+
+// Function to populate the form fields with data from a clicked table row
+$('table').on('click', 'tbody tr', function() {
+
+    let itemCodeValue = $(this).find('th').text();
+    let itemNameValue = $(this).find('td:eq(0)').text();
+    let priceValue = $(this).find('td:eq(1)').text();
+    let qtyOnHandValue = $(this).find('td:eq(2)').text();
+
+    itemCode.val(itemCodeValue);
+    itemName.val(itemNameValue);
+    price.val(priceValue);
+    QtyOnHand.val(qtyOnHandValue);
+
+});
+
+/*Customer Form Update*/
+update.on('click', () => {
+
+    let itemCodeValue = parseInt(itemCode.val(), 10);
+    let itemNameValue = itemName.val().trim();
+    let priceValue = price.val().trim();
+    let qtyOnHandValue = QtyOnHand.val().trim();
+
+    if(
+        validation(itemNameValue, "item name", null) &&
+        validation(priceValue, "Price", null) &&
+        validation(qtyOnHandValue, "Qty On Hand",null)){
+
+        item_db.map((item) => {
+            if (item.item_code === itemCodeValue) {
+                item.item_name = itemNameValue;
+                item.price = priceValue;
+                item.qty_on_hand = qtyOnHandValue;
+            }
+
+        });
+
+        Swal.fire(
+            'Update Successfully !',
+            'Successful',
+            'success'
+        )
+
+        populateItemTable();
+
+        resetColumns();
+
+    }
+
+});
+
+/*Customer Form Reset*/
+reset.on('click', function(e) {
+    e.preventDefault();
+    itemCode.val(generateItemCode());
+    itemName.val('');
+    price.val('');
+    QtyOnHand.val('');
+});
+
+/*Customer Form Delete*/
+delete_btn.on('click', () => {
+
+    let itemCodeValue = parseInt(itemCode.val(), 10);
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Delete'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let index = item_db.findIndex(item => item.item_code === itemCodeValue);
+            item_db.splice(index, 1);
+            populateItemTable();
+            reset.click();
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+    });
+
+
+});
+
